e1cf25c61d1046cebcdf4d1224b52882f94b7ad3
Robert Marco <bjm3491@rit.edu>
2021-04-27 21:14:48 -0400

    recommend by recent

--- utils/recommendations.py
+++ utils/recommendations.py
@@ -71,3 +71,31 @@ def recommend_by_user(userid):
         if conn is not None:
             conn.close()
     return results
+
+
+def recommend_by_recent():
+    """ finds recipe based on search """
+    checkdb = """SELECT "RecipeId", "RecipeName", "CreationDate"
+                 FROM "Recipes"
+                 ORDER BY "CreationDate" DESC;"""
+    conn = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # check if user exists
+        cur.execute(checkdb)
+        # store all results
+        results = cur.fetchall()
+        # close the cursor
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return results
+
