eed26e7619b0cd397e8ab0ed865176604535d11d
Robert Marco <bjm3491@rit.edu>
2021-04-28 22:09:36 -0400

    small fix to spacing

--- utils/recommendations.py
+++ utils/recommendations.py
@@ -38,18 +38,18 @@ def recommend_by_user(userid):
     """ finds recipe based on search """
     checkdb = """SELECT DISTINCT "Ratings"."RecipeId", "RecipeName", "avg" FROM
                      (SELECT "Recipes"."RecipeId", "Recipes"."RecipeName", ROUND(AVG("CookedRecipes"."Rating") ,2) AS "avg"
-                     FROM "Recipes" INNER JOIN "CookedRecipes"
-                     ON  "Recipes"."RecipeId" = "CookedRecipes"."RecipeId"
-                     GROUP BY "Recipes"."RecipeId") AS "Ratings", 
+                        FROM "Recipes" INNER JOIN "CookedRecipes"
+                        ON  "Recipes"."RecipeId" = "CookedRecipes"."RecipeId"
+                        GROUP BY "Recipes"."RecipeId") AS "Ratings", 
                      (SELECT DISTINCT "CookedRecipes"."UserId" FROM
-                     (SELECT DISTINCT "RecipeId", "UserId" FROM "CookedRecipes" 
-                     WHERE "UserId" = '{}') AS "WasMade", "CookedRecipes"
-                     WHERE "WasMade"."RecipeId" = "CookedRecipes"."RecipeId" 
-                     AND "WasMade"."UserId" != "CookedRecipes"."UserId") AS "OtherUsers", "CookedRecipes"
-                     WHERE "OtherUsers"."UserId" = "CookedRecipes"."UserId" AND 
-                     "CookedRecipes"."RecipeId" NOT IN (SELECT DISTINCT "RecipeId" FROM "CookedRecipes" 
-                     WHERE "UserId" = '{}') AND "Ratings"."RecipeId" = "CookedRecipes"."RecipeId"
-                     ORDER BY "avg" DESC;""".format(userid, userid)
+                        (SELECT DISTINCT "RecipeId", "UserId" FROM "CookedRecipes" 
+                            WHERE "UserId" = '{}') AS "WasMade", "CookedRecipes"
+                        WHERE "WasMade"."RecipeId" = "CookedRecipes"."RecipeId" 
+                        AND "WasMade"."UserId" != "CookedRecipes"."UserId") AS "OtherUsers", "CookedRecipes"
+                    WHERE "OtherUsers"."UserId" = "CookedRecipes"."UserId" AND 
+                    "CookedRecipes"."RecipeId" NOT IN 
+                        (SELECT DISTINCT "RecipeId" FROM "CookedRecipes" WHERE "UserId" = '{}') 
+                    AND "Ratings"."RecipeId" = "CookedRecipes"."RecipeId" ORDER BY "avg" DESC;""".format(userid, userid)
 
     conn = None
     try:
